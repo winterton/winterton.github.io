1d0b62c117c5d4136abfe9b30697fa4b0c34454c
Jacob Smith <winterton@me.com>
2018-05-19 11:36:17 -0400

    Make notes optional.

--- rsvp.js
+++ rsvp.js
@@ -7,16 +7,33 @@ $(function() {
             data: JSON.stringify(formToJson(this)),
             dataType: "json",
             method:"POST",
-            contentType:"application/json"
+            contentType:"application/json",
+            success: function(data, status, xhr) {
+                resultDebounceSubmitButton("Sent!", "success");
+            },
+            error: function(data, status, xhr) {
+                resultDebounceSubmitButton("Failed to send. Please try again.", "error");
+            }
         });
     });
 });
+
+function resultDebounceSubmitButton(stateText, resultClass) {
+    $('#submit').val(stateText);
+    $('#submit').attr('disabled', 'disabled');
+    $('#submit').addClass(resultClass);
+    setTimeout(function() {
+        $('#submit').val("Submit");
+        $('#submit').removeAttr('disabled');
+        $('#submit').removeClass(resultClass);
+    }, 5000);
+}
 
 function formToJson(form) {
     var json = {};
     var array = $(form).serializeArray();
     $.each(array, function(i, e) {
-        json[e.name] = e.value || "";
+        json[e.name] = e.value || "N/A";
     });
     return json;
 }
